a8cb2d6a057e66dd2fd5678d646b014d03915ffa
Jillian Pichocki <jpichocki@gmail.com>
2017-08-09 17:20:08 -0500

    search function works

--- main.js
+++ main.js
@@ -1,26 +1,15 @@
-/*
-  Here is a rough idea for the steps you could take:
-*/
-
-// 1. First select and store the elements you'll be working with
-// 2. Create your `submit` event for getting the user's search term
-// 3. Create your `fetch` request that is called after a submission
-// 4. Create a way to append the fetch results to your page
-// 5. Create a way to listen for a click that will play the song in the audio play
-
-
-
-// var displayArea = document.querySelector(".images");
 var input = document.querySelector("#input");
 var submit = document.querySelector("#submit");
 var resultsDisplay = document.querySelector(".mainContent");
 
-submit.addEventListener("click", ourCallback());
+submit.addEventListener("click", function (e) {
 
-function ourCallback(e) {
-  // if (e.keyCode === 13) {
+  let url = "https://itunes.apple.com/search?term=";
+  url += input.value;
 
-  axios.get(`https://itunes.apple.com/search?term==${input.value}`)
+  e.preventDefault();
+
+  axios.get(url)
     .then(function (response) {
       let results = response.data.results;
       resultsDisplay.innerHTML = " ";
@@ -48,29 +37,6 @@ function ourCallback(e) {
       }
 
     });
-
-}
-  // fetch(`https://itunes.apple.com/search?term==${input.value}`)
-  //   .then(function (response) {
-  //     response.json().then(function (data) {
-  //       let results = data.results;
-  //       resultsDisplay.innerHTML = " ";
-
-      // })
-    // });
-
-// return data.json().then(function(data) {
+});
 
 
-
-      // <a href ="${href}" target="_blank"><img src="https://s3-ap-southeast-1.amazonaws.com/assets.limetray.com/assets/user_images/menus/original/1442837463_Imgggg.jpg"><div class="caption">${title}</div></a>`;
-
-
-    // });
-// }
-// }
-
-// search.addEventListener("keypress", ourCallback);
-// search.addEventListener("click", function () {
-//   displayArea.innerHTML = "";
-//   search.value = "";
