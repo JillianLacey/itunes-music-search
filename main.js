dbe4cf5ee192cc3ec91e51bcf4b6656cfaa61c0e
Jillian Pichocki <jpichocki@gmail.com>
2017-08-09 16:38:46 -0500

    populate array info in js

--- main.js
+++ main.js
@@ -7,3 +7,70 @@
 // 3. Create your `fetch` request that is called after a submission
 // 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
+
+
+
+// var displayArea = document.querySelector(".images");
+var input = document.querySelector("#input");
+var submit = document.querySelector("#submit");
+var resultsDisplay = document.querySelector(".mainContent");
+
+submit.addEventListener("click", ourCallback());
+
+function ourCallback(e) {
+  // if (e.keyCode === 13) {
+
+  axios.get(`https://itunes.apple.com/search?term==${input.value}`)
+    .then(function (response) {
+      let results = response.data.results;
+      resultsDisplay.innerHTML = " ";
+
+      for (var i = 0; i < results.length; i++) {
+        let data = results[i];
+        let artistName = results[i].artistName;
+        let albumArtwork = results[i].artworkUrl100;
+        let trackName = results[i].trackName;
+
+
+        resultsDisplay.innerHTML += `
+
+          <div class="container-results">
+            <div class="image-parent">
+              <img src="${albumArtwork}">
+            </div>
+
+            <div class="content-parent">
+              <p>${artistName}</p>
+              <p>${trackName}</p>
+            </div>
+          </div>
+          `
+      }
+
+    });
+
+}
+  // fetch(`https://itunes.apple.com/search?term==${input.value}`)
+  //   .then(function (response) {
+  //     response.json().then(function (data) {
+  //       let results = data.results;
+  //       resultsDisplay.innerHTML = " ";
+
+      // })
+    // });
+
+// return data.json().then(function(data) {
+
+
+
+      // <a href ="${href}" target="_blank"><img src="https://s3-ap-southeast-1.amazonaws.com/assets.limetray.com/assets/user_images/menus/original/1442837463_Imgggg.jpg"><div class="caption">${title}</div></a>`;
+
+
+    // });
+// }
+// }
+
+// search.addEventListener("keypress", ourCallback);
+// search.addEventListener("click", function () {
+//   displayArea.innerHTML = "";
+//   search.value = "";
